425db23e056606a4a67db1c1e911817cc7be5b4a
bord81 <bslabut@gmail.com>
2019-08-02 10:11:34 +0300

    Add files via upload

--- simple.kt
+++ simple.kt
@@ -2,12 +2,13 @@ sealed class SimType {
     fun isReducible(): Boolean {
         return when (this) {
             is NumberSim -> false
+            is DoNothing -> false
             else -> true
         }
     }
 
-    open fun reduce(env: Map<String, SimType>): SimType {
-        return this
+    open fun reduce(env: Map<String, SimType>): Pair<SimType, Map<String, SimType>> {
+        return Pair(this, mapOf())
     }
 
     open fun value(): Int {
@@ -33,11 +34,11 @@ class AddSim(left: SimType, right: SimType) : SimType() {
         return "$left + $right"
     }
 
-    override fun reduce(env: Map<String, SimType>): SimType {
+    override fun reduce(env: Map<String, SimType>): Pair<SimType, Map<String, SimType>> {
         return when {
-            left.isReducible() -> AddSim(left.reduce(env), right)
-            right.isReducible() -> AddSim(left, right.reduce(env))
-            else -> NumberSim(left.value() + right.value())
+            left.isReducible() -> Pair(AddSim(left.reduce(env).first, right), env)
+            right.isReducible() -> Pair(AddSim(left, right.reduce(env).first), env)
+            else -> Pair(NumberSim(left.value() + right.value()), env)
         }
     }
 }
@@ -49,45 +50,85 @@ class MultiplySim(left: SimType, right: SimType) : SimType() {
         return "$left * $right"
     }
 
-    override fun reduce(env: Map<String, SimType>): SimType {
+    override fun reduce(env: Map<String, SimType>): Pair<SimType, Map<String, SimType>> {
         return when {
-            left.isReducible() -> MultiplySim(left.reduce(env), right)
-            right.isReducible() -> MultiplySim(left, right.reduce(env))
-            else -> NumberSim(left.value() * right.value())
+            left.isReducible() -> Pair(MultiplySim(left.reduce(env).first, right), env)
+            right.isReducible() -> Pair(MultiplySim(left, right.reduce(env).first), env)
+            else -> Pair(NumberSim(left.value() * right.value()), env)
         }
     }
 }
 
-class Variable(name: String) :SimType() {
+class Variable(name: String) : SimType() {
     private val name = name
     override fun toString(): String {
         return "$name"
     }
 
-    override fun reduce(env: Map<String, SimType>): SimType {
-        return env[name] ?: error("$name is not available in environment")
+    override fun reduce(env: Map<String, SimType>): Pair<SimType, Map<String, SimType>> {
+        if (env.containsKey(name)) {
+            return Pair(env[name], env) as Pair<SimType, Map<String, SimType>>
+        } else error("$name is not available in environment")
+    }
+}
+
+class DoNothing : SimType() {
+    override fun toString(): String {
+        return "do-nothing"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return other is DoNothing
+    }
+}
+
+class Assign(name: String, expr: SimType) : SimType() {
+    private val name = name
+    private val expr = expr
+    override fun toString(): String {
+        return "$name = $expr"
+    }
+
+    override fun reduce(env: Map<String, SimType>): Pair<SimType, Map<String, SimType>> {
+        return if (expr.isReducible()) {
+            Pair(Assign(name, expr.reduce(env).first), env)
+        } else {
+            val temp = env.toMutableMap()
+            temp[name] = expr
+            Pair(DoNothing(), temp.toMap())
+        }
     }
 }
 
 class Machine(expr: SimType, env: Map<String, SimType>) {
     private var expr = expr
-    private val env = env
+    private var env = env
     private fun step() {
-        expr = expr.reduce(env)
+        val eval = expr.reduce(env)
+        expr = eval.first
+        env = eval.second
     }
 
     fun run() {
         while (expr.isReducible()) {
             println(expr)
+            println(env)
             step()
         }
         println(expr)
+        println(env)
     }
 }
 
 fun main() {
     val testExpr = AddSim(Variable("x"), Variable("y"))
-    val environment = mapOf("x" to NumberSim(3), "y" to NumberSim(4))
+    val environment = mutableMapOf("x" to NumberSim(3), "y" to NumberSim(4))
     val vm = Machine(testExpr, environment)
     vm.run()
+
+    println()
+    val testExpr2 = Assign("x", AddSim(Variable("x"), NumberSim(1)))
+    val environment2 = mutableMapOf("x" to NumberSim(2))
+    val vm2 = Machine(testExpr2, environment2)
+    vm2.run()
 }
