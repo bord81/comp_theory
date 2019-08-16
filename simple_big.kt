1a4c6adab1cf431acd033d9ea073cd44d6391811
bobr <bslabut@gmail.com>
2019-08-16 09:33:51 +0300

    Updated big-step semantics

--- simple_big.kt
+++ simple_big.kt
@@ -2,8 +2,8 @@ package comp_theory
 
 /* Simple language using big step semantics */
 private sealed class SimBigType {
-    open fun evaluate(env: Map<String, SimBigType>): SimBigType {
-        return this
+    open fun evaluate(env: Map<String, SimBigType>): Pair<SimBigType, Map<String, SimBigType>> {
+        return Pair(this, mapOf())
     }
 
     open fun value(): Int {
@@ -29,8 +29,8 @@ private class NumberBigSim(value: Int) : SimBigType() {
         return value != 0
     }
 
-    override fun evaluate(env: Map<String, SimBigType>): SimBigType {
-        return this
+    override fun evaluate(env: Map<String, SimBigType>): Pair<SimBigType, Map<String, SimBigType>> {
+        return Pair(this, env)
     }
 }
 
@@ -48,8 +48,8 @@ private class BoolBigSim(value: Boolean) : SimBigType() {
         return value
     }
 
-    override fun evaluate(env: Map<String, SimBigType>): SimBigType {
-        return this
+    override fun evaluate(env: Map<String, SimBigType>): Pair<SimBigType, Map<String, SimBigType>> {
+        return Pair(this, env)
     }
 }
 
@@ -59,8 +59,8 @@ private class VarBigSim(name: String) : SimBigType() {
         return "$name"
     }
 
-    override fun evaluate(env: Map<String, SimBigType>): SimBigType {
-        return env[name]!!
+    override fun evaluate(env: Map<String, SimBigType>): Pair<SimBigType, Map<String, SimBigType>> {
+        return Pair(env[name]!!, env)
     }
 }
 
@@ -71,8 +71,8 @@ private class AddBigSim(left: SimBigType, right: SimBigType) : SimBigType() {
         return "$left + $right"
     }
 
-    override fun evaluate(env: Map<String, SimBigType>): SimBigType {
-        return NumberBigSim(left.evaluate(env).value() + right.evaluate(env).value())
+    override fun evaluate(env: Map<String, SimBigType>): Pair<SimBigType, Map<String, SimBigType>> {
+        return Pair(NumberBigSim(left.evaluate(env).first.value() + right.evaluate(env).first.value()), env)
     }
 }
 
@@ -83,8 +83,8 @@ private class MultBigSim(left: SimBigType, right: SimBigType) : SimBigType() {
         return "$left * $right"
     }
 
-    override fun evaluate(env: Map<String, SimBigType>): SimBigType {
-        return NumberBigSim(left.evaluate(env).value() * right.evaluate(env).value())
+    override fun evaluate(env: Map<String, SimBigType>): Pair<SimBigType, Map<String, SimBigType>> {
+        return Pair(NumberBigSim(left.evaluate(env).first.value() * right.evaluate(env).first.value()), env)
     }
 }
 
@@ -95,17 +95,106 @@ private class LessBigSim(left: SimBigType, right: SimBigType) : SimBigType() {
         return "$left < $right"
     }
 
-    override fun evaluate(env: Map<String, SimBigType>): SimBigType {
-        return BoolBigSim(left.evaluate(env).value() < right.evaluate(env).value())
+    override fun evaluate(env: Map<String, SimBigType>): Pair<SimBigType, Map<String, SimBigType>> {
+        return Pair(BoolBigSim(left.evaluate(env).first.value() < right.evaluate(env).first.value()), env)
+    }
+}
+
+private class AssignBigSim(name: String, expr: SimBigType) : SimBigType() {
+    private val name = name
+    private val expr = expr
+    override fun toString(): String {
+        return "$name = $expr"
+    }
+
+    override fun evaluate(env: Map<String, SimBigType>): Pair<SimBigType, Map<String, SimBigType>> {
+        val temp = env.toMutableMap()
+        temp[name] = expr.evaluate(env).first
+        return Pair(DoNothingBigSim(), temp)
+    }
+
+}
+
+private class DoNothingBigSim : SimBigType() {
+    override fun toString(): String {
+        return "do-nothing"
+    }
+
+    override fun evaluate(env: Map<String, SimBigType>): Pair<SimBigType, Map<String, SimBigType>> {
+        return Pair(this, env)
+    }
+}
+
+private class IfBigSim(condition: SimBigType, consequence: SimBigType, alternative: SimBigType) : SimBigType() {
+    private val cond = condition
+    private val conseq = consequence
+    private val alt = alternative
+    override fun toString(): String {
+        return "if $cond then $conseq else $alt"
+    }
+
+    override fun evaluate(env: Map<String, SimBigType>): Pair<SimBigType, Map<String, SimBigType>> {
+        return if (cond.evaluate(env).first.boolValue()) {
+            conseq.evaluate(env)
+        } else {
+            alt.evaluate(env)
+        }
+    }
+}
+
+private class SequenceBigSim(first: SimBigType, second: SimBigType) : SimBigType() {
+    private val first = first
+    private val second = second
+    override fun toString(): String {
+        return "$first; $second;"
+    }
+
+    override fun evaluate(env: Map<String, SimBigType>): Pair<SimBigType, Map<String, SimBigType>> {
+        return second.evaluate(first.evaluate(env).second)
+    }
+}
+
+private class WhileBigSim(condition: SimBigType, body: SimBigType) : SimBigType() {
+    private val cond = condition
+    private val body = body
+    override fun toString(): String {
+        return "while($cond) {$body}"
+    }
+
+    override fun evaluate(env: Map<String, SimBigType>): Pair<SimBigType, Map<String, SimBigType>> {
+        var result_env: MutableMap<String, SimBigType>
+        if (cond.evaluate(env).first.boolValue()) {
+            result_env = evaluate(body.evaluate(env).second).second.toMutableMap()
+        } else {
+            result_env = env.toMutableMap()
+        }
+        return Pair(this, result_env)
     }
 }
 
 fun main() {
     println(NumberBigSim(5).evaluate(mutableMapOf()))
+
     println(VarBigSim("x").evaluate(mutableMapOf(Pair("x", NumberBigSim(20)))))
+
     println(AddBigSim(NumberBigSim(5), NumberBigSim(8)).evaluate(mutableMapOf()))
+
     println(
         LessBigSim(AddBigSim(VarBigSim("x"), NumberBigSim(2)), VarBigSim("y"))
             .evaluate(mutableMapOf(Pair("x", NumberBigSim(2)), Pair("y", NumberBigSim(5))))
     )
+
+    val statement1 = SequenceBigSim(
+        AssignBigSim("x", AddBigSim(NumberBigSim(1), NumberBigSim(1))),
+        AssignBigSim("y", AddBigSim(VarBigSim("x"), NumberBigSim(3)))
+    )
+    println(statement1)
+    println(statement1.evaluate(mutableMapOf()))
+
+    val statement2 = WhileBigSim(
+        LessBigSim(VarBigSim("x"), NumberBigSim(5)),
+        AssignBigSim("x", MultBigSim(VarBigSim("x"), NumberBigSim(3)))
+    )
+    println(statement2)
+    println(statement2.evaluate(mutableMapOf(Pair("x", NumberBigSim(1)))))
 }
